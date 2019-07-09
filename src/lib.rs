fb40439d4fa41632779c0a84767343e07152cfc2
Collin Valley <collin.valley@gmail.com>
2019-07-09 08:03:17 -0700

    Improve test coverage and verbosity
    This commit adds 3 additional tests, driven by a LinearIntervalGenerator
    and clarifies a naming convention.  We also add a short bit of
    documentation to the first test to explain the name.  All tests pass at
    this commit point.

--- src/lib.rs
+++ src/lib.rs
@@ -29,8 +29,13 @@ mod tests {
         }
     }
 
+    /// One Synchronous Element, sourced with an interval yield
+    /// 
+    /// This test creates one Sync element, and uses the LinearIntervalGenerator to test whether
+    /// the element responds correctly to an upstream source providing a series of valid packets,
+    /// interleaved with Async::NotReady values, finalized by a Async::Ready(None)
     #[test]
-    fn impl_sync_element_works() {
+    fn one_sync_element_interval_yield() {
         let packet_generator = LinearIntervalGenerator::new(time::Duration::from_millis(100), 10);
 
         let elem1 = TrivialElement { id: 0 };
@@ -63,7 +68,7 @@ mod tests {
 
 
     #[test]
-    fn one_async_element_immediate_notify() {
+    fn one_async_element_immediate_yield() {
         let default_channel_size = 10;
         let packet_generator = iter_ok::<_, ()>(0..20);
 
@@ -82,7 +87,7 @@ mod tests {
     }
 
     #[test]
-    fn two_async_elements_immediate_notify() {
+    fn two_async_elements_immediate_yield() {
         let default_channel_size = 10;
         let packet_generator = iter_ok::<_, ()>(0..20);
 
@@ -106,9 +111,80 @@ mod tests {
     }
 
     #[test]
-    fn series_sync_and_async_immediate_notify() {
+    fn series_sync_and_async_immediate_yield() {
         let default_channel_size = 10;
         let packet_generator = iter_ok::<_, ()>(0..20);
+
+        let elem0 = TrivialElement { id: 0 };
+        let elem1 = AsyncTrivialElement { id: 1 };
+        let elem2 = TrivialElement { id: 2 };
+        let elem3 = AsyncTrivialElement { id: 3 };
+
+        let elem0_link = ElementLink::new(Box::new(packet_generator), elem0);
+        let elem1_link = AsyncElementLink::new(Box::new(elem0_link), elem1, default_channel_size);
+        let elem2_link = ElementLink::new(Box::new(elem1_link.backend), elem2);
+        let elem3_link = AsyncElementLink::new(Box::new(elem2_link), elem3, default_channel_size);
+
+        let elem1_drain = ExhaustiveDrain::new(0, Box::new(elem1_link.frontend));
+        let elem3_drain = ExhaustiveDrain::new(1, Box::new(elem3_link.frontend));
+
+        let elem3_consumer = ExhaustiveDrain::new(2, Box::new(elem3_link.backend));
+
+        tokio::run(lazy (|| {
+            tokio::spawn(elem1_drain);
+            tokio::spawn(elem3_drain); 
+            tokio::spawn(elem3_consumer);
+            Ok(())
+        }));
+    }
+
+        #[test]
+    fn one_async_element_interval_yield() {
+        let default_channel_size = 10;
+        let packet_generator = LinearIntervalGenerator::new(time::Duration::from_millis(100), 20);
+
+        let elem0 = AsyncTrivialElement { id: 0 };
+
+        let elem0_link = AsyncElementLink::new(Box::new(packet_generator), elem0, default_channel_size);
+
+        let elem0_drain = ExhaustiveDrain::new(0, Box::new(elem0_link.frontend));
+        let elem0_consumer = ExhaustiveDrain::new(1, Box::new(elem0_link.backend));
+
+        tokio::run(lazy (|| {
+            tokio::spawn(elem0_drain);
+            tokio::spawn(elem0_consumer);
+            Ok(())
+        }));
+    }
+
+    #[test]
+    fn two_async_elements_interval_yield() {
+        let default_channel_size = 10;
+        let packet_generator = LinearIntervalGenerator::new(time::Duration::from_millis(100), 20);
+
+        let elem0 = AsyncTrivialElement { id: 0 };
+        let elem1 = AsyncTrivialElement { id: 1 };
+
+        let elem0_link = AsyncElementLink::new(Box::new(packet_generator), elem0, default_channel_size);
+        let elem1_link = AsyncElementLink::new(Box::new(elem0_link.backend), elem1, default_channel_size);
+
+        let elem0_drain = ExhaustiveDrain::new(0, Box::new(elem0_link.frontend));
+        let elem1_drain = ExhaustiveDrain::new(1, Box::new(elem1_link.frontend));
+
+        let elem1_consumer = ExhaustiveDrain::new(1, Box::new(elem1_link.backend));
+
+        tokio::run(lazy (|| {
+            tokio::spawn(elem0_drain);
+            tokio::spawn(elem1_drain);
+            tokio::spawn(elem1_consumer);
+            Ok(())
+        }));
+    }
+
+    #[test]
+    fn series_sync_and_async_interval_yield() {
+        let default_channel_size = 10;
+        let packet_generator = LinearIntervalGenerator::new(time::Duration::from_millis(100), 20);
 
         let elem0 = TrivialElement { id: 0 };
         let elem1 = AsyncTrivialElement { id: 1 };
