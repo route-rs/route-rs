7b9c92931306c2328fd455ec1e1979c9b8ab9638
Collin Valley <collin.valley@gmail.com>
2019-07-14 17:47:23 -0700

    packet_collectors.rs:  Impl Exhaustive Colllector
    Exhaustive Collector is a packet collector that will grab every packet
    out of a stream, but it is different from Exhaustive Drain in that it
    writes the collected packet into the provided channel handle. This is
    useful because the test may then inspect the channel to determine
    whether the test was a pass for fail.

--- src/utils/test/packet_collectors.rs
+++ src/utils/test/packet_collectors.rs
@@ -1,6 +1,7 @@
 use crate::api::ElementStream;
 use futures::{Async, Poll, Future};
 use std::fmt::Debug;
+use crossbeam::crossbeam_channel;
 
 pub struct ExhaustiveDrain<T: Debug> {
     id: usize,
@@ -27,6 +28,42 @@ impl<T: Debug> Future for ExhaustiveDrain<T> {
                 },
                 None => {
                     println!("Drain #{} received none. End of packet stream", self.id);
+                    return Ok(Async::Ready(()))
+                }
+            }
+        }
+    }
+}
+
+/// Exhaustive Collector works like Exhaustive Drain in that it continously polls for packets until it
+/// receives a None, but it also will write that value out to the provided channel, so that the packet
+/// may be compared in a test.
+pub struct ExhaustiveCollector<T: Debug> {
+    id: usize,
+    stream: ElementStream<T>,
+    packet_dump: crossbeam_channel::Sender<T>
+}
+
+impl<T: Debug> ExhaustiveCollector<T> {
+    pub fn new(id: usize, stream: ElementStream<T>, packet_dump: crossbeam_channel::Sender<T>) -> Self {
+        ExhaustiveCollector { id, stream, packet_dump }
+    }
+}
+
+impl<T: Debug> Future for ExhaustiveCollector<T> {
+    type Item = ();
+    type Error = ();
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        loop {
+            match try_ready!(self.stream.poll()) {
+                Some(value) => {
+                    if let Err(err) = self.packet_dump.try_send(value) {
+                        println!("Exhaustive Collector: Error sending to packet dump: {:?}", err);
+                    };
+                },
+                None => {
+                    println!("Collector #{} received none. End of packet stream", self.id);
                     return Ok(Async::Ready(()))
                 }
             }
