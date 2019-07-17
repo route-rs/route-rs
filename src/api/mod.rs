e2f3575f94f93a719b45f930c3e1ac146c7ec577
Collin Valley <collin.valley@gmail.com>
2019-07-17 00:21:01 -0700

    join_element.rs: Implement JoinElement
    This join element works as essentially the mirror to the classify
    element. It has many consumers, each with a channel on which they
    can place packets to be pulled by the single provider. This
    implementation also requires an overseer component, and it's drop
    logic is somewhat different. It keeps track of which consumers are
    counted as dead by substituting a None for ther wake_consumer
    Receiver<task::Task>. This is done to ensure that a counted dead
    consumer is not double counted. The overseer iterates over each
    packet_channel, and will attempt to wake the provider if it detects
    that any of the channels contain a packet.
    
    Issues: This join is unfair, since the provider always starts with the
    first consumer, rather than executing a try round robin. This will be
    resolved before we merge this element.

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -3,5 +3,6 @@ use futures::Stream;
 pub mod element;
 pub mod async_element;
 pub mod classify_element;
+pub mod join_element;
 
 pub type ElementStream<Input> = Box<dyn Stream<Item = Input, Error = ()> + Send>;
