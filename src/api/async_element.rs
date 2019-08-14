b70b507d044040ef7c31a4205b0365c9db2f80f2
Collin Valley <collin.valley@gmail.com>
2019-08-13 22:24:20 -0700

    Async Element: Shorten test with immediate stream
    This is a small change to one of our tests that is rather long
    running, at least compared to other tests. Switching to immediate
    stream when we are not testing packet delay improves the speed.

--- src/api/async_element.rs
+++ src/api/async_element.rs
@@ -285,10 +285,7 @@ mod tests {
     fn one_async_element_small_channel() {
         let default_channel_size = 1;
         let packets = vec![0, 1, 2, 420, 1337, 3, 4, 5, 6, 7, 8, 9];
-        let packet_generator = PacketIntervalGenerator::new(
-            time::Duration::from_millis(100),
-            packets.clone().into_iter(),
-        );
+        let packet_generator = immediate_stream(packets.clone());
 
         let elem0 = AsyncIdentityElement::new(0);
 
