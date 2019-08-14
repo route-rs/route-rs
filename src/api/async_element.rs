f69a5c558896c86c4dd5daf021d7578b1a0db3ca
Drew Dowling <drewdowling37@gmail.com>
2019-08-13 23:32:23 -0700

    fix: decrease packet interval from 100 to 10

--- src/api/async_element.rs
+++ src/api/async_element.rs
@@ -286,7 +286,7 @@ mod tests {
         let default_channel_size = 1;
         let packets = vec![0, 1, 2, 420, 1337, 3, 4, 5, 6, 7, 8, 9];
         let packet_generator = PacketIntervalGenerator::new(
-            time::Duration::from_millis(100),
+            time::Duration::from_millis(10),
             packets.clone().into_iter(),
         );
 
