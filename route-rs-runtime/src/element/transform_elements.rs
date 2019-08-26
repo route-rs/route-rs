aaf1e4276081ac5d3c88922742031ef5c3a33e17
Ethan Everett <github@ethaneverett.com>
2019-08-25 23:14:56 -0700

    TransformElements: new elem w/ different I/O types
    Add a new trivial element for testing that has a different input and
    output type. Use std::convert::From to do so generically.
    
    Closes #63

---
+++ route-rs-runtime/src/element/transform_elements.rs
@@ -0,0 +1,40 @@
+use crate::element::{AsyncElement, Element};
+use std::convert::From;
+use std::marker::PhantomData;
+
+/*
+  TransformElement
+  This is an element that takes type A and passes the equivalent type B using From
+*/
+#[derive(Default)]
+pub struct TransformElement<A: Sized, B: Sized> {
+    phantom_in: PhantomData<A>,
+    phantom_out: PhantomData<B>,
+}
+
+impl<A, B> TransformElement<A, B> {
+    pub fn new() -> TransformElement<A, B> {
+        TransformElement {
+            phantom_in: PhantomData,
+            phantom_out: PhantomData,
+        }
+    }
+}
+
+impl<A, B: From<A>> Element for TransformElement<A, B> {
+    type Input = A;
+    type Output = B;
+
+    fn process(&mut self, packet: Self::Input) -> Self::Output {
+        Self::Output::from(packet)
+    }
+}
+
+impl<A, B: From<A>> AsyncElement for TransformElement<A, B> {
+    type Input = A;
+    type Output = B;
+
+    fn process(&mut self, packet: Self::Input) -> Self::Output {
+        Self::Output::from(packet)
+    }
+}
