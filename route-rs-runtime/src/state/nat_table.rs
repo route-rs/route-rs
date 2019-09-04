3780a8afae24b8449f56482329c2d8d4b5cd9d9f
Collin Valley <collin.valley@gmail.com>
2019-09-03 20:59:39 -0700

    Add one good test, one failing test

--- route-rs-runtime/src/state/nat_table.rs
+++ route-rs-runtime/src/state/nat_table.rs
@@ -66,4 +66,69 @@ impl NatTable {
         let nat_table = self.table.read().unwrap();
         nat_table.contains_right(external)
     }
+
+    /// Returns number of entries in the table
+    pub fn len(&self) -> usize {
+        let nat_table = self.table.read().unwrap();
+        nat_table.len()
+    }
+
+    /// Returns true if the table is empty
+    pub fn is_empty(&self) -> bool {
+        let nat_table = self.table.read().unwrap();
+        nat_table.is_empty()
+    }
+
+    /// Clears all entries in the table
+    pub fn clear(&self) {
+        let mut nat_table = self.table.write().unwrap();
+        nat_table.clear();
+    }
+}
+
+#[cfg(test)]
+#[allow(dead_code)]
+mod tests {
+    use super::*;
+    use crate::packet::tuple::LookupTupleIpv4;
+    use smoltcp::phy::ChecksumCapabilities;
+    use smoltcp::wire::*;
+
+    #[test]
+    fn create_empty_table() {
+        let nat_table = NatTable::new();
+        assert!(nat_table.is_empty());
+    }
+
+    #[test]
+    fn insert_one_row() {
+        let nat_table = NatTable::new();
+
+        // Create a testing packet
+        let init_ttl = 64;
+        let repr = Ipv4Repr {
+            src_addr: Ipv4Address::new(10, 0, 0, 1),
+            dst_addr: Ipv4Address::new(10, 0, 0, 2),
+            protocol: IpProtocol::Tcp,
+            payload_len: 10,
+            hop_limit: init_ttl,
+        };
+        let buffer = vec![0; repr.buffer_len() + repr.payload_len];
+        let mut packet = Ipv4Packet::new_unchecked(buffer);
+        repr.emit(&mut packet, &ChecksumCapabilities::default());
+
+        // Create test tuple
+        //TODO: this fails because I don't have a valid TCP packet
+        let mut internal_tuple = LookupTupleIpv4::new(&mut packet).unwrap();
+        let mut external_tuple = internal_tuple.clone();
+
+        // Test insertion
+        nat_table.insert(internal_tuple.clone(), external_tuple.clone()).unwrap();
+        assert_eq!(nat_table.len(), 1);
+
+        // Test contains
+        assert!(nat_table.contains_internal(&internal_tuple));
+        assert!(nat_table.contains_external(&external_tuple));
+
+    }
 }
