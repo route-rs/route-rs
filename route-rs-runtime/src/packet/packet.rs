04f247dad52a2cb2b0bfc9a0ac8fdf6b3b798f70
Collin Valley <collin.valley@gmail.com>
2019-09-17 21:49:43 -0700

    Ethernet: Improve code for MacAddr access
    This uses less ugly array semantics for MacAddr, still has magic
    numbers in it though.

--- route-rs-runtime/src/packet/packet.rs
+++ route-rs-runtime/src/packet/packet.rs
@@ -1,31 +1,21 @@
 //Let's use this area for now to declare common structs, constants, and common helper functions.
 
-//Most significant byte is b0
+//Most significant byte is 0th
 #[derive(Eq, Clone, Copy, Hash, PartialEq)]
 pub struct MacAddr {
-    pub b0: u8,
-    pub b1: u8,
-    pub b2: u8,
-    pub b3: u8,
-    pub b4: u8,
-    pub b5: u8,
+    pub bytes: [u8; 6],
 }
 
 impl MacAddr {
-    pub fn new(b0: u8, b1: u8, b2: u8, b3: u8, b4: u8, b5: u8) -> MacAddr {
+    pub fn new(bytes: [u8; 6]) -> MacAddr {
         MacAddr {
-            b0,
-            b1,
-            b2,
-            b3,
-            b4,
-            b5,
+            bytes,
         }
     }
 }
 
 
-//Most significant byte is b0
+//Most significant byte is 0th
 #[derive(Eq, Clone, Copy, Hash, PartialEq)]
 pub struct Ipv4Addr {
     pub bytes: [u8; 4],
