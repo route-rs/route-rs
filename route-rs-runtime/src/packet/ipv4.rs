66c060f7c6ce0ab5ae6d0ce0793584ad85a6f5db
Collin Valley <collin.valley@gmail.com>
2019-09-17 22:21:32 -0700

    Ipv4: Add header_len function, properly calculate payload offset

--- route-rs-runtime/src/packet/ipv4.rs
+++ route-rs-runtime/src/packet/ipv4.rs
@@ -1,7 +1,6 @@
 use crate::packet::*;
 use std::convert::TryFrom;
 
-
 pub struct Ipv4Packet<'packet> {
     pub data: PacketData<'packet>,
     payload_offset: usize,
@@ -27,7 +26,11 @@ impl<'packet> Ipv4Packet<'packet> {
             return Err("Packet has invalid total len field");
         }
 
-        Ok(Ipv4Packet { data: packet, payload_offset: 14 + 20 })
+        //This is the header length in 32bit words
+        let internet_header_len = (packet[14] & 0x0F) as usize;
+        let payload_offset = (internet_header_len*4) + 14;
+
+        Ok(Ipv4Packet { data: packet, payload_offset: payload_offset})
     }
 
     //MAGIC ALERT, src addr offset (12) and Ipv4 header offset
@@ -44,7 +47,6 @@ impl<'packet> Ipv4Packet<'packet> {
 
     //MAGIC ALERT, dest addr offset (16) and Ipv4 header offset
     pub fn dest_addr(&self) -> Ipv4Addr {
-        //This makes us an array from a slice.
         let bytes = <[u8; 4]>::try_from(&self.data[(14+16)..(14+19)]).unwrap();
         Ipv4Addr::new(bytes)
     }
@@ -52,12 +54,14 @@ impl<'packet> Ipv4Packet<'packet> {
     pub fn set_dest_addr(&mut self, addr: Ipv4Addr) {
         for i in 0..4 {
             self.data[14 + 16 + i] = addr.bytes[i];
-        }        
+        }     
+    }
+
+    pub fn header_len(&self) -> u8 {
+        self.data[14] & 0x0F
     }
 
 
-    //Version u4 -> should be 0100, for ipv4
-    //HeaderLen u4 -> header length in 32bit words, min total 20bytes
     //TypeofService u8 ->  RFC2474, some QoS stuff
     //TotalLen u16 -> total length in bytes, min is 20 bytes, max is 65353, since well 16 bits
     //Identification u16 -> Fragment ID, identifies fragmented IP for reassembly later.
@@ -65,8 +69,6 @@ impl<'packet> Ipv4Packet<'packet> {
     //TTL u8 -> Should be dropped it zero
     //Protocol u8 -> 1 ICMP, 6 TCP, 17 UDP, 41 IPv6 tun over ipv4
     //Header Checksum u16 -> Ones compliment of the ones complement sum of all 16 bit words in the header
-    //src_ip u32
-    //dst_ip 32
     //options (0 - 40 bytes), this will be annoying, hardly ever used "in the wild"
     //Payload
 
