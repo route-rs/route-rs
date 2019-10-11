5fcf873831dee62756e3b4bb8138269427f0852f
Collin Valley <collin.valley@gmail.com>
2019-10-10 21:56:14 -0700

    AsyncLink: Move to builder pattern
    Moves AsyncLink to builder pattern, and adds some additional required
    trait bounds to element. This has to do with things requiring to be Send
    when being put into a channel, and with the traits the compiler is no
    longer able to infer that the E::Input and E::Output are Send. So we
    state that as a fact.

--- route-rs-runtime/src/element/base_element.rs
+++ route-rs-runtime/src/element/base_element.rs
@@ -1,6 +1,6 @@
 pub trait Element {
-    type Input: Sized;
-    type Output: Sized;
+    type Input: Sized + Send;
+    type Output: Sized + Send;
 
     fn process(&mut self, packet: Self::Input) -> Option<Self::Output>;
 }
