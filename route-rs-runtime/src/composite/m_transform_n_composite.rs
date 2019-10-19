72a9ee47c751d5924c8d43de31816f1bdafc3276
Collin Valley <collin.valley@gmail.com>
2019-10-18 21:37:51 -0700

    Composite: M transform N
    This composite takes M streams of type Input, uses a provided element
    that maps Input->Ouput, and then provides N egressors that implement
    Stream of type Output. Pretty good example of the power of
    composite links. Not completely sold on the name

--- route-rs-runtime/src/composite/m_transform_n_composite.rs
+++ route-rs-runtime/src/composite/m_transform_n_composite.rs
@@ -1,9 +1,10 @@
-use crate::link::{CloneLink, JoinLink, Link, LinkBuilder, PacketStream};
 use crate::element::Element;
-use crate::link::SyncLinkBuilder;
+use crate::link::{
+    CloneLink, ElementLinkBuilder, JoinLink, Link, LinkBuilder, PacketStream, SyncLinkBuilder,
+};
 
-#[derive(default)]
-pub struct MtransformNComposite<E: Element> {
+#[derive(Default)]
+pub struct MtransformNComposite<E: Element + Send> {
     in_streams: Option<Vec<PacketStream<E::Input>>>,
     element: Option<E>,
     join_queue_capacity: usize,
@@ -11,7 +12,7 @@ pub struct MtransformNComposite<E: Element> {
     num_egressors: Option<usize>,
 }
 
-impl<E: Element> MtransformNComposite<E> {
+impl<E: Element + Send> MtransformNComposite<E> {
     pub fn new() -> Self {
         MtransformNComposite {
             in_streams: None,
@@ -27,7 +28,10 @@ impl<E: Element> MtransformNComposite<E> {
     pub fn join_queue_capacity(self, queue_capacity: usize) -> Self {
         assert!(
             (1..=1000).contains(&queue_capacity),
-            format!("join_queue_capacity: {} must be in range 1..=1000", queue_capacity)
+            format!(
+                "join_queue_capacity: {} must be in range 1..=1000",
+                queue_capacity
+            )
         );
 
         MtransformNComposite {
@@ -44,7 +48,10 @@ impl<E: Element> MtransformNComposite<E> {
     pub fn clone_queue_capacity(self, queue_capacity: usize) -> Self {
         assert!(
             (1..=1000).contains(&queue_capacity),
-            format!("clone_queue_capacity: {} must be in range 1..=1000", queue_capacity)
+            format!(
+                "clone_queue_capacity: {} must be in range 1..=1000",
+                queue_capacity
+            )
         );
 
         MtransformNComposite {
@@ -63,7 +70,7 @@ impl<E: Element> MtransformNComposite<E> {
         );
         assert_ne!(num_egressors, 0, "num_egressors must be non-zero");
 
-        MtoNComposite {
+        MtransformNComposite {
             in_streams: self.in_streams,
             element: self.element,
             join_queue_capacity: self.join_queue_capacity,
@@ -73,8 +80,8 @@ impl<E: Element> MtransformNComposite<E> {
     }
 }
 
-impl<E:Element + Send + 'static> LinkBuilder<E::Input, E::Output> for MtransformNComposite<E> {
-    fn ingressors(self, in_streams: Vec<PacketStream<E>>) -> Self {
+impl<E: Element + Send + 'static> LinkBuilder<E::Input, E::Output> for MtransformNComposite<E> {
+    fn ingressors(self, in_streams: Vec<PacketStream<E::Input>>) -> Self {
         assert!(
             in_streams.len() > 1 && in_streams.len() <= 1000,
             format!("Input streams {} not in 2..=1000", in_streams.len())
@@ -103,9 +110,9 @@ impl<E:Element + Send + 'static> LinkBuilder<E::Input, E::Output> for Mtransform
 
             let (sync_runnables, sync_egressors) = SyncLinkBuilder::new()
                 .ingressors(join_egressors)
-                .element(self.element)
+                .element(self.element.unwrap())
                 .build_link();
-            
+
             let (mut clone_link_runnables, clone_link_egressors) = CloneLink::new()
                 .ingressors(sync_egressors)
                 .queue_capacity(self.clone_queue_capacity)
@@ -113,7 +120,7 @@ impl<E:Element + Send + 'static> LinkBuilder<E::Input, E::Output> for Mtransform
                 .build_link();
             clone_link_runnables.extend(join_runnables);
             clone_link_runnables.extend(sync_runnables);
-            
+
             (clone_link_runnables, clone_link_egressors)
         }
     }
@@ -127,7 +134,7 @@ impl<E: Element + Send + 'static> ElementLinkBuilder<E> for MtransformNComposite
             join_queue_capacity: self.join_queue_capacity,
             clone_queue_capacity: self.clone_queue_capacity,
             num_egressors: self.num_egressors,
-        }        
+        }
     }
 }
 
@@ -135,10 +142,12 @@ impl<E: Element + Send + 'static> ElementLinkBuilder<E> for MtransformNComposite
 #[allow(dead_code)]
 mod tests {
     use super::*;
+    use crate::element::TransformElement;
     use crate::link::{LinkBuilder, TokioRunnable};
     use crate::utils::test::packet_collectors::ExhaustiveCollector;
     use crate::utils::test::packet_generators::immediate_stream;
     use crossbeam::crossbeam_channel;
+    use std::net::Ipv4Addr;
 
     use futures::future::lazy;
 
@@ -152,19 +161,23 @@ mod tests {
     }
 
     #[test]
-    fn MtransformN_Composite() {
-        let packets = vec![0, 1, 2, 420, 1337, 3, 4, 5, 6, 7, 8, 9, 11];
+    fn m_transform_n_composite() {
+        let packets = vec![0xDEADBEEF, 0xBEEFDEAD, 0x0A000001, 0xFFFFFFFF];
         let number_num_egressors = 2;
         let packet_generator0 = immediate_stream(packets.clone());
         let packet_generator1 = immediate_stream(packets.clone());
 
-        let mut input_streams: Vec<PacketStream<usize>> = Vec::new();
+        let mut input_streams: Vec<PacketStream<u32>> = Vec::new();
         input_streams.push(Box::new(packet_generator0));
         input_streams.push(Box::new(packet_generator1));
 
-        let (mut runnables, mut egressors) = MtoNComposite::new()
+        //Simple element that calls the From<u32> function of Ipv4Addr over all inputs.
+        let transform_element = TransformElement::<u32, Ipv4Addr>::new();
+
+        let (mut runnables, mut egressors) = MtransformNComposite::new()
             .num_egressors(number_num_egressors)
             .ingressors(input_streams)
+            .element(transform_element)
             .build_link();
 
         let (s1, collector1_output) = crossbeam_channel::unbounded();
--- route-rs-runtime/src/composite/mod.rs
+++ route-rs-runtime/src/composite/mod.rs
@@ -2,3 +2,8 @@
 /// type to N packets.
 mod mton_composite;
 pub use self::mton_composite::*;
+
+/// More complex composite that will take in N ingress streams of type Input,
+/// and transforms it using a user provided element to M Output streams.
+mod m_transform_n_composite;
+pub use self::m_transform_n_composite::*;
