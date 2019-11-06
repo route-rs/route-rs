3d93d9eb4dceceb10853703a2730f71099610987
Sam Gruber <sam@scgruber.com>
2019-11-05 18:39:19 -0800

    graphgen: Reenable example tests
    Now that we have fixed graphgen for the examples, reenable the tests so
    we stop breaking them more.

--- route-rs-graphgen/tests/tests.rs
+++ route-rs-graphgen/tests/tests.rs
@@ -1,25 +1,27 @@
 #[allow(dead_code)]
 mod test_helper;
 
-// TODO: re-enable after Link API is stabilized
-//#[test]
-//fn trivial_identity() {
-//    let test_helper = test_helper::TestHelper::new(
-//        "trivial-identity",
-//        vec!["--rustfmt", "--local-modules", "packets"],
-//    );
-//
-//    test_helper.run_graphgen();
-//    test_helper.run_diff();
-//}
-//
-//#[test]
-//fn dns_interceptor() {
-//    let test_helper = test_helper::TestHelper::new(
-//        "dns-interceptor",
-//        vec!["--rustfmt", "--runtime-modules", "link"],
-//    );
-//
-//    test_helper.run_graphgen();
-//    test_helper.run_diff();
-//}
+#[test]
+fn trivial_identity() {
+    let test_helper = test_helper::TestHelper::new(
+        "trivial-identity",
+        vec![
+            "--rustfmt",
+            "--local-modules",
+            "packets",
+            "--runtime-modules",
+            "processor",
+        ],
+    );
+
+    test_helper.run_graphgen();
+    test_helper.run_diff();
+}
+
+#[test]
+fn dns_interceptor() {
+    let test_helper = test_helper::TestHelper::new("dns-interceptor", vec!["--rustfmt"]);
+
+    test_helper.run_graphgen();
+    test_helper.run_diff();
+}
