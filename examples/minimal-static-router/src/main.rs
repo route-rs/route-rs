041f1cb47b6c8dc2e80301f88a2d14800fb565d2
Collin Valley <collin.valley@gmail.com>
2019-11-07 22:57:47 -0800

    Create skeleton main with a  struct that implements build_link, this is the think we will actually call run_tokio on

--- examples/minimal-static-router/src/main.rs
+++ examples/minimal-static-router/src/main.rs
@@ -1,27 +1,69 @@
-use route_rs_packets::{EthernetFrame, Ipv6Packet, Ipv4Packet};
+use route_rs_packets::{EthernetFrame};
 use route_rs_runtime::processor::Processor;
+use crossbeam::crossbeam_channel;
+use route_rs_runtime::pipeline::Runner;
+use route_rs_runtime::link::{
+    primitive::{ForkLink, JoinLink},
+    Link, LinkBuilder, PacketStream,
+};
+
+// I'd really like to use something like this.
+// use route_rs_runtime::utils::test::harness::run_link;
+// use route_rs_runtime::utils::test::packet_generators::{immediate_stream, PacketIntervalGenerator};
 
 mod processors;
 
 fn main() {
-    let data: Vec<u8> =
-        vec![0xde, 0xad, 0xbe, 0xef, 0xff, 0xff, 1, 2, 3, 4, 5, 6, 0, 0,
-        0x60, 0, 0, 0, 0, 4, 17, 64, 0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef, 0xde,
-        0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-        14, 15, 0xa, 0xb, 0xc, 0xd,
-    ];
-    let frame = EthernetFrame::new(data).unwrap();
+    let to_ipv6 = processors::Ipv6Encap;
+    let to_ipv4 = processors::Ipv4Encap;
+    
+    let (input_packet_sender, input_receiver) = crossbeam_channel::unbounded();
+    let input_packets: Vec<EthernetFrame> = vec![]; //Put packets here.
 
-    let mut processor = processors::Ipv6Encap;
-
-    let packet = processor.process(frame);
-
-    match packet {
-        Some(packet) => {
-            println!("Got ipv6packet: src_addr = {}", packet.src_addr());
+    for p in input_packets {
+        match input_packet_sender.send(p) {
+            Ok(_) => {},
+            Err(err) => panic!("Input channel error {}", err),
         }
-        None => {
-            println!("Some error occured");
+    }
+
+    // Then we call our runner on our router
+
+    // Then we collect the outputs and see if it works
+}
+
+#[derive(Default)]
+pub struct Router<EthernetFrame> {
+    in_streams: Option<Vec<PacketStream<EthernetFrame>>>,
+}
+
+impl <EthernetFrame> Router<EthernetFrame> {
+    pub fn new() -> Self {
+        Router {
+            in_streams: None,
         }
     }
 }
+
+impl<EthernetFrame>LinkBuilder<EthernetFrame,EthernetFrame> for Router<EthernetFrame> {
+    fn ingressors(self, in_streams: Vec<PacketStream<EthernetFrame>>) -> Self {
+        assert!(in_streams.len() == 1, "Support only one input interface for now");
+        Router {
+            in_streams: Some(in_streams),
+        }
+    }
+
+    fn build_link(self) -> Link<EthernetFrame> {
+        if self.in_streams.is_none() {
+            panic!("Can not build link, missing input stream");
+        } else {
+            // TODO: Build the router here
+
+
+            //return an empty thing for now so it compiles.
+            (vec![], vec![])
+        }
+    }
+}
+
+
