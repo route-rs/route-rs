871c4f34681c91c0f28bfc055a9796bd2d150637
Collin Valley <collin.valley@gmail.com>
2019-11-12 20:24:58 -0800

    MSR: Create first basic test for a processor
    Test the Ipv4Decap. This is a primite test though, since we don't
    implement Eq or PartialEq, I can't compare the output with anything
    meaningful. Next step is to do that in Packets

--- examples/minimal-static-router/src/processors.rs
+++ examples/minimal-static-router/src/processors.rs
@@ -57,3 +57,33 @@ impl Processor for Ipv4Encap {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use route_rs_packets::EthernetFrame;
+    use route_rs_runtime::link::primitive::ProcessLink;
+    use route_rs_runtime::link::{LinkBuilder, ProcessLinkBuilder};
+    use route_rs_runtime::utils::test::harness::run_link;
+    use route_rs_runtime::utils::test::packet_generators::immediate_stream;
+
+    #[test]
+    fn decap_ipv4() {
+        let data: Vec<u8> = vec![
+            0xde, 0xad, 0xbe, 0xef, 0xff, 0xff, 1, 2, 3, 4, 5, 6, 08, 00, //MAC layer
+            0x45, 0, 0, 20, 0, 0, 0, 0, 64, 17, 0, 0, 192, 178, 128, 0, 10, 0, 0, 1, //IPLayer
+        ];
+        let frame = EthernetFrame::new(data.clone(), 0);
+        let packets = vec![frame.unwrap()];
+
+        let link = ProcessLink::new()
+            .ingressor(immediate_stream(packets))
+            .processor(Ipv4Decap)
+            .build_link();
+
+        let results = run_link(link);
+
+        // I should implement Eq for all the packets, basically just check if they have the same underlying data.
+        assert_eq!(results[0].len(), 1);
+    }
+}
