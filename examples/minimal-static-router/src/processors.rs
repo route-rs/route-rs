21d28ebbdc4f6eb863aa51634a68177c97e8edfe
Collin Valley <collin.valley@gmail.com>
2019-11-12 21:56:36 -0800

    MSR: Implement basic tests for encap,decap processors

--- examples/minimal-static-router/src/processors.rs
+++ examples/minimal-static-router/src/processors.rs
@@ -71,10 +71,11 @@ mod tests {
     fn decap_ipv4() {
         let data: Vec<u8> = vec![
             0xde, 0xad, 0xbe, 0xef, 0xff, 0xff, 1, 2, 3, 4, 5, 6, 08, 00, //MAC layer
-            0x45, 0, 0, 20, 0, 0, 0, 0, 64, 17, 0, 0, 192, 178, 128, 0, 10, 0, 0, 1, //IPLayer
+            0x45, 0, 0, 20, 0, 0, 0, 0, 64, 17, 0, 0, 192, 178, 128, 0, 10, 0, 0,
+            1, //IP Layer
         ];
-        let frame = EthernetFrame::new(data.clone(), 0);
-        let packets = vec![frame.unwrap()];
+        let frame = EthernetFrame::new(data.clone(), 0).unwrap();
+        let packets = vec![frame];
 
         let link = ProcessLink::new()
             .ingressor(immediate_stream(packets))
@@ -85,5 +86,70 @@ mod tests {
 
         let test_packet = Ipv4Packet::new(data, Some(0), 14).unwrap();
         assert_eq!(results[0][0], test_packet);
+    }
+
+    #[test]
+    fn encap_ipv4() {
+        let data: Vec<u8> = vec![
+            0xde, 0xad, 0xbe, 0xef, 0xff, 0xff, 1, 2, 3, 4, 5, 6, 08, 00, //MAC layer
+            0x45, 0, 0, 20, 0, 0, 0, 0, 64, 17, 0, 0, 192, 178, 128, 0, 10, 0, 0,
+            1, //IP Layer
+        ];
+        let packet = Ipv4Packet::new(data.clone(), Some(0), 14).unwrap();
+        let packets = vec![packet];
+
+        let link = ProcessLink::new()
+            .ingressor(immediate_stream(packets))
+            .processor(Ipv4Encap)
+            .build_link();
+
+        let results = run_link(link);
+
+        let test_frame = EthernetFrame::new(data, 0).unwrap();
+        assert_eq!(results[0][0], test_frame);
+    }
+
+    #[test]
+    fn decap_ipv6() {
+        let data: Vec<u8> = vec![
+            0xde, 0xad, 0xbe, 0xef, 0xff, 0xff, 1, 2, 3, 4, 5, 6, 0x86, 0xDD, //MAC layer
+            0x60, 0, 0, 0, 0, 4, 17, 64, 0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef,
+            0xde,0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+            14, 15, 0xa, 0xb, 0xc, 0xd,
+        ];
+        let frame = EthernetFrame::new(data.clone(), 0).unwrap();
+        let packets = vec![frame];
+
+        let link = ProcessLink::new()
+            .ingressor(immediate_stream(packets))
+            .processor(Ipv6Decap)
+            .build_link();
+
+        let results = run_link(link);
+
+        let test_packet = Ipv6Packet::new(data, Some(0), 14).unwrap();
+        assert_eq!(results[0][0], test_packet);
+    }
+
+    #[test]
+    fn encap_ipv6() {
+        let data: Vec<u8> = vec![
+            0xde, 0xad, 0xbe, 0xef, 0xff, 0xff, 1, 2, 3, 4, 5, 6, 0x86, 0xDD, //MAC layer
+            0x60, 0, 0, 0, 0, 4, 17, 64, 0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef,
+            0xde,0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+            14, 15, 0xa, 0xb, 0xc, 0xd,
+        ];
+        let packet = Ipv6Packet::new(data.clone(), Some(0), 14).unwrap();
+        let packets = vec![packet];
+
+        let link = ProcessLink::new()
+            .ingressor(immediate_stream(packets))
+            .processor(Ipv6Encap)
+            .build_link();
+
+        let results = run_link(link);
+
+        let test_frame = EthernetFrame::new(data, 0).unwrap();
+        assert_eq!(results[0][0], test_frame);
     }
 }
