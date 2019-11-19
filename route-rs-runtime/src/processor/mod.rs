a929787f4a9a12c7b467f5e594cec801b674c326
Collin Valley <collin.valley@gmail.com>
2019-11-18 21:09:40 -0800

    Runtime: Improve documentation
    This cafeteria commit improves many aspects of the documentation across
    the runtime. While the docs are not complete, this will help provide a
    good future foundation.

--- route-rs-runtime/src/processor/mod.rs
+++ route-rs-runtime/src/processor/mod.rs
@@ -1,3 +1,12 @@
+//! # What are they for?
+//!
+//! Processors are the unit of transformation in route-rs. The Processor is defined by a trait that requires that
+//! creators of processors implement a process function, that the underlying Link will call on every packet that
+//! moves through it. You can use processors to modify headers, count packets, update state, create new packets,
+//! join packets, whatever you want! As long as an processor conforms to the trait specification, the processor can be run inside a route-rs.
+//! While there are many provided processors that can be used to implement a router, users of route-rs that need specifc functionality
+//! in their router most likely will implement their own custom processors, conforming to the laid out processor standard.
+
 mod identity;
 pub use self::identity::*;
 
@@ -10,6 +19,7 @@ pub use self::drop::*;
 mod dec_ip_hop;
 pub use self::dec_ip_hop::*;
 
+/// In order for a struct to become a Processor, it must implement this Trait.
 pub trait Processor {
     type Input: Send + Clone;
     type Output: Send + Clone;
