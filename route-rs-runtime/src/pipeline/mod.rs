18f9939486ef8ee88af7b4305f50c29b670e8db3
Collin Valley <collin.valley@gmail.com>
2019-11-21 21:26:09 -0800

    One more change

--- route-rs-runtime/src/pipeline/mod.rs
+++ route-rs-runtime/src/pipeline/mod.rs
@@ -1,8 +1,5 @@
 //! # What are they?
 //!
-//! Pipelines are abstractions used by graphgen to define a grouping of links that are run by the runtime, in our case Tokio. The graphgen
-//! program takes the user provided graph, and generates a pipeline. It connects input and output channels to the pipe, so that it can be
-//! hooked up to packet sources and sinks, and drops the pipeline into the runtime. Generally can be abstracted to the concept, router.
-
+//! Pipelines are abstractions used by graphgen to IO packets for a router through channels.
 mod runner;
 pub use self::runner::*;
