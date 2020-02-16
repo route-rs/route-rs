d5166b59220c6fe6371cc6fd3ee81fef8dc6f63d
Collin Valley <collin.valley@gmail.com>
2020-02-16 13:40:36 -0800

    Runtime: runner() now takes a Trait Object
    Before we were taking an function object that could produce us a Link
    that could be run in the tokio runtime, instead now we take a Trait
    Object that implements LinkBuilder. Since it implements LinkBuilder, we
    can assume that the object passed in is already initialized, and we then
    build the object inside the Tokio runtime context by calling
    build_link(). This is much more natural than the previous
    implementation, and helps to eliminate some boilerplate.

--- route-rs-runtime/src/utils/runner.rs
+++ route-rs-runtime/src/utils/runner.rs
@@ -1,4 +1,4 @@
-use crate::link::{Link, TokioRunnable};
+use crate::link::{TokioRunnable, LinkBuilder};
 use crate::utils::test::packet_collectors::ExhaustiveCollector;
 use crossbeam::crossbeam_channel;
 use std::fmt::Debug;
@@ -7,9 +7,9 @@ use tokio::task::JoinHandle;
 
 /// Runner is a user facing helper function for running the constructed router.
 ///
-/// Its only argument is a function pointer that takes no arguments and returns a Link type. This
-/// master link should contain all the runnables and outputs of the router, which in turn allows
-/// this function to initialize and start the Router.
+/// Runner takes only one argument, router, which is a Trait Object that implements LinkBuilder. Runner will construct
+/// the router inside of the Runtime context by calling build_link(). As such, the router object that is passed in should
+/// do all of the required initialization before it is passed into runner. (IE calling ingressors(), etc)
 ///
 /// In general, the `Link` returned by the router should contain only TokioRunnables and no PacketStreams,
 /// since production routers are self contained with all their output going to links that push the packets
@@ -19,9 +19,9 @@ use tokio::task::JoinHandle;
 /// hook them into `PacketCollector` links, and whatever packets come out of those egressors will be returned to you once
 /// the router completes operation and joins. In a production router, the router likely never stops running so
 /// nothing will ever get returned.  Use this functionality only for testing.  
-pub fn runner<OutputPacket: Debug + Send + Clone + 'static>(
-    link_builder: fn() -> Link<OutputPacket>,
-) -> Vec<Vec<OutputPacket>> {
+pub fn runner<Packet: Debug + Send + Clone + 'static, Router: LinkBuilder<Packet, Packet>>(
+    router: Router,
+) -> Vec<Vec<Packet>> {
     let mut runtime = runtime::Builder::new()
         .threaded_scheduler()
         .enable_all()
@@ -29,15 +29,15 @@ pub fn runner<OutputPacket: Debug + Send + Clone + 'static>(
         .unwrap();
 
     runtime.block_on(async {
-        let (mut runnables, egressors) = link_builder();
+        let (mut runnables, egressors) = router.build_link();
 
         let (mut consumers, receivers): (
             Vec<TokioRunnable>,
-            Vec<crossbeam_channel::Receiver<OutputPacket>>,
+            Vec<crossbeam_channel::Receiver<Packet>>,
         ) = egressors
             .into_iter()
             .map(|egressor| {
-                let (s, r) = crossbeam_channel::unbounded::<OutputPacket>();
+                let (s, r) = crossbeam_channel::unbounded::<Packet>();
                 // TODO: Do we care about consumer IDs? Are they helpful to debug test examples?
                 let consumer: TokioRunnable = Box::new(ExhaustiveCollector::new(0, egressor, s));
                 (consumer, r)
