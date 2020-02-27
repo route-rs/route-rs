fdd105c31a3086b0b6f7f5bc12a4bcaa082510fe
Collin Valley <collin.valley@gmail.com>
2020-02-26 21:28:09 -0800

    PHR: Rework Intake and Exhaust Links
    At our last meeting, other members brought up that we should not code
    the IO layers into the business logic of our core router, particularly
    since we want to be able to test the router using PCAPs, and swapping
    real OS interfaces for PCAP streams shouldn't be a pain. So I am
    reworking the intake and exhaust to not do this, and to handle
    converting the raw Vec<u8> packet streams to route-rs Packets.

--- examples/pi-home-router/src/interface/link/mod.rs
+++ examples/pi-home-router/src/interface/link/mod.rs
@@ -5,5 +5,6 @@ pub(crate) use self::interface_dispatch::InterfaceDispatch;
 mod interface_collect;
 #[allow(unused_imports)]
 pub(crate) use self::interface_collect::InterfaceCollect;
-//mod router_intake;
-//mod router_exhaust;
+
+mod router_exhaust;
+mod router_intake;
