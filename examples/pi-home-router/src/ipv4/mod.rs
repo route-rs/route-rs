29b9b6eb6d786573c38e3b72e3dcbb79fc80cddc
Sam Gruber <sam@scgruber.com>
2020-03-08 13:04:04 -0700

    PHR IPV4: Drop spurious packets from the LAN
    Packets from the LAN should be destined for the Host (or broadcast) or
    the WAN. Any packets that we receive destined for other LAN addresses
    are an error, and we should drop them.
    
    In order to implement this, we now have to know what our LAN IP and
    subnet are, so we add fields and builders for those.
    
    Note that we have to handle both the global broadcast 255.255.255.255
    and the local broadcast which is (network | (broadcast & subnet mask)).
    Fortunately, cidr provides .last_address() which does this for us.

--- examples/pi-home-router/src/ipv4/mod.rs
+++ examples/pi-home-router/src/ipv4/mod.rs
@@ -16,6 +16,8 @@ use classify_dest_subnet::ByDestSubnet;
 pub(crate) struct HandleIpv4 {
     in_stream: Option<PacketStream<InterfaceAnnotated<Ipv4Packet>>>,
     wan_ip: Option<Ipv4Addr>,
+    lan_ip: Option<Ipv4Addr>,
+    lan_subnet: Option<Ipv4Cidr>,
 }
 
 #[allow(dead_code)]
@@ -24,6 +26,8 @@ impl HandleIpv4 {
         HandleIpv4 {
             in_stream: None,
             wan_ip: None,
+            lan_ip: None,
+            lan_subnet: None,
         }
     }
 
@@ -32,8 +36,34 @@ impl HandleIpv4 {
             None => HandleIpv4 {
                 in_stream: self.in_stream,
                 wan_ip: Some(wan_ip),
+                lan_ip: self.lan_ip,
+                lan_subnet: self.lan_subnet,
             },
             Some(_) => panic!("HandleIPv4 takes only one wan_ip"),
+        }
+    }
+
+    pub(crate) fn lan_ip(self, lan_ip: Ipv4Addr) -> Self {
+        match self.lan_ip {
+            None => HandleIpv4 {
+                in_stream: self.in_stream,
+                wan_ip: self.wan_ip,
+                lan_ip: Some(lan_ip),
+                lan_subnet: self.lan_subnet,
+            },
+            Some(_) => panic!("HandleIPv4 takes only one lan_ip"),
+        }
+    }
+
+    pub(crate) fn lan_subnet(self, lan_subnet: Ipv4Cidr) -> Self {
+        match self.lan_subnet {
+            None => HandleIpv4 {
+                in_stream: self.in_stream,
+                wan_ip: self.wan_ip,
+                lan_ip: self.lan_ip,
+                lan_subnet: Some(lan_subnet),
+            },
+            Some(_) => panic!("HandleIPv4 takes only one lan_subnet"),
         }
     }
 }
@@ -47,6 +77,8 @@ impl LinkBuilder<InterfaceAnnotated<Ipv4Packet>, InterfaceAnnotated<Ipv4Packet>>
             HandleIpv4 {
                 in_stream: Some(in_streams.remove(0)),
                 wan_ip: self.wan_ip,
+                lan_ip: self.lan_ip,
+                lan_subnet: self.lan_subnet,
             }
         } else {
             panic!("HandleIPv4 takes exactly one ingressor");
@@ -58,6 +90,8 @@ impl LinkBuilder<InterfaceAnnotated<Ipv4Packet>, InterfaceAnnotated<Ipv4Packet>>
             None => HandleIpv4 {
                 in_stream: Some(in_stream),
                 wan_ip: self.wan_ip,
+                lan_ip: self.lan_ip,
+                lan_subnet: self.lan_subnet,
             },
             Some(_) => panic!("HandleIPv4 takes only one ingressor"),
         }
@@ -71,6 +105,14 @@ impl LinkBuilder<InterfaceAnnotated<Ipv4Packet>, InterfaceAnnotated<Ipv4Packet>>
         assert!(
             self.wan_ip.is_some(),
             "HandleIPv4 must have a wan_ip defined"
+        );
+        assert!(
+            self.lan_ip.is_some(),
+            "HandleIPv4 must have a lan_ip defined"
+        );
+        assert!(
+            self.lan_subnet.is_some(),
+            "HandleIPv4 must have a lan_subnet defined"
         );
 
         let mut all_runnables = vec![];
@@ -88,6 +130,30 @@ impl LinkBuilder<InterfaceAnnotated<Ipv4Packet>, InterfaceAnnotated<Ipv4Packet>>
             }))
             .build_link();
         unpack_link!(classify_by_inbound_interface => all_runnables, [from_host, from_lan, from_wan]);
+
+        // Packets from the LAN are either bound for the Host (or LAN Broadcast), else they go out
+        // to the WAN. If we receive packets bound for the LAN, we discard them because we don't
+        // want to reflect traffic back into the LAN link.
+        let classify_by_dest_subnet_lan = ClassifyLink::new()
+            .ingressor(from_lan)
+            .classifier(ByDestSubnet::new(
+                maplit::hashmap! {
+                    Ipv4Cidr::new(self.lan_ip.unwrap(), 32).unwrap() => FromLanDestSubnet::MyLanIp,
+                    Ipv4Cidr::new(Ipv4Addr::new(255,255,255,255), 32).unwrap() => FromLanDestSubnet::Broadcast,
+                    Ipv4Cidr::new(self.lan_subnet.clone().unwrap().last_address(), 32).unwrap() => FromLanDestSubnet::Broadcast,
+                    self.lan_subnet.clone().unwrap() => FromLanDestSubnet::Lan,
+                },
+                FromLanDestSubnet::Other,
+            ))
+            .num_egressors(2)
+            .dispatcher(Box::new(|subnet| match subnet {
+                FromLanDestSubnet::MyLanIp => Some(0),
+                FromLanDestSubnet::Broadcast => Some(0),
+                FromLanDestSubnet::Lan => None,
+                FromLanDestSubnet::Other => Some(1),
+            }))
+            .build_link();
+        unpack_link!(classify_by_dest_subnet_lan => all_runnables, [from_lan_to_host, from_lan_to_wan_annotated]);
 
         // Discard all packets from the WAN that aren't destined for my WAN IP. We only have one WAN
         // address, so anything else that comes from the WAN is an error by upstream. We don't care
@@ -139,12 +205,25 @@ impl LinkBuilder<InterfaceAnnotated<Ipv4Packet>, InterfaceAnnotated<Ipv4Packet>>
 
         // Join everything so we have one outbound packet stream
         let final_join = JoinLink::new()
-            .ingressors(vec![from_host, from_lan, from_nat_decap_annotated])
+            .ingressors(vec![
+                from_host,
+                from_lan_to_host,
+                from_lan_to_wan_annotated,
+                from_nat_decap_annotated,
+            ])
             .build_link();
         unpack_link!(final_join => all_runnables, final_join_egressors);
 
         (all_runnables, final_join_egressors)
     }
+}
+
+#[derive(Clone)]
+enum FromLanDestSubnet {
+    MyLanIp,
+    Lan,
+    Broadcast,
+    Other,
 }
 
 #[derive(Clone)]
@@ -161,16 +240,24 @@ mod tests {
 
     const MY_WAN_IP: Ipv4Addr = Ipv4Addr::new(192, 0, 2, 1);
     const OTHER_WAN_IP: Ipv4Addr = Ipv4Addr::new(192, 0, 2, 101);
+    const MY_LAN_IP: Ipv4Addr = Ipv4Addr::new(10, 0, 21, 67);
+    // This is awkward because we can't call Cidr::new(...).unwrap() in a const expression
+    const MY_LAN_SUBNET_ADDR: Ipv4Addr = Ipv4Addr::new(10, 0, 21, 64);
+    const MY_LAN_SUBNET_LEN: u8 = 26;
 
     fn test_handleipv4_link(
         packets: Vec<InterfaceAnnotated<Ipv4Packet>>,
         wan_ip: Ipv4Addr,
+        lan_ip: Ipv4Addr,
+        lan_subnet: Ipv4Cidr,
     ) -> Vec<InterfaceAnnotated<Ipv4Packet>> {
         let mut runtime = initialize_runtime();
         let mut results = runtime.block_on(async {
             let link = HandleIpv4::new()
                 .ingressor(immediate_stream(packets))
                 .wan_ip(wan_ip)
+                .lan_ip(lan_ip)
+                .lan_subnet(lan_subnet)
                 .build_link();
             test_link(link, None).await
         });
@@ -189,8 +276,12 @@ mod tests {
             inbound_interface: Interface::Unmarked,
             outbound_interface: Interface::Unmarked,
         };
-
-        let output_packets = test_handleipv4_link(vec![from_host, from_unmarked], MY_WAN_IP);
+        let output_packets = test_handleipv4_link(
+            vec![from_host, from_unmarked],
+            MY_WAN_IP,
+            MY_LAN_IP,
+            Cidr::new(MY_LAN_SUBNET_ADDR, MY_LAN_SUBNET_LEN).unwrap(),
+        );
 
         assert_eq!(output_packets.len(), 1);
         assert!(output_packets
@@ -215,8 +306,12 @@ mod tests {
             outbound_interface: Interface::Unmarked,
         };
 
-        let output_packets =
-            test_handleipv4_link(vec![from_wan_to_me, from_wan_to_other], MY_WAN_IP);
+        let output_packets = test_handleipv4_link(
+            vec![from_wan_to_me, from_wan_to_other],
+            MY_WAN_IP,
+            MY_LAN_IP,
+            Cidr::new(MY_LAN_SUBNET_ADDR, MY_LAN_SUBNET_LEN).unwrap(),
+        );
 
         assert_eq!(output_packets.len(), 1);
         assert!(output_packets
@@ -234,12 +329,104 @@ mod tests {
             outbound_interface: Interface::Unmarked,
         };
 
-        let output_packets = test_handleipv4_link(vec![from_wan_to_me], MY_WAN_IP);
+        let output_packets = test_handleipv4_link(
+            vec![from_wan_to_me],
+            MY_WAN_IP,
+            MY_LAN_IP,
+            Cidr::new(MY_LAN_SUBNET_ADDR, MY_LAN_SUBNET_LEN).unwrap(),
+        );
 
         assert_eq!(output_packets.len(), 1);
         assert_eq!(
             output_packets.first().unwrap().outbound_interface,
             Interface::Lan
         );
+    }
+
+    #[test]
+    fn from_lan_to_lan_is_dropped() {
+        let mut from_lan_to_lan_packet = Ipv4Packet::empty();
+        from_lan_to_lan_packet.set_dest_addr(Ipv4Addr::new(10, 0, 21, 99));
+        let from_lan_to_lan = InterfaceAnnotated {
+            packet: from_lan_to_lan_packet,
+            inbound_interface: Interface::Lan,
+            outbound_interface: Interface::Unmarked,
+        };
+
+        let output_packets = test_handleipv4_link(
+            vec![from_lan_to_lan],
+            MY_WAN_IP,
+            MY_LAN_IP,
+            Cidr::new(MY_LAN_SUBNET_ADDR, MY_LAN_SUBNET_LEN).unwrap(),
+        );
+
+        assert_eq!(output_packets.len(), 0);
+    }
+
+    #[test]
+    fn from_lan_to_me_isnt_dropped() {
+        let mut from_lan_to_me_packet = Ipv4Packet::empty();
+        from_lan_to_me_packet.set_dest_addr(MY_LAN_IP);
+        let from_lan_to_me = InterfaceAnnotated {
+            packet: from_lan_to_me_packet,
+            inbound_interface: Interface::Lan,
+            outbound_interface: Interface::Unmarked,
+        };
+
+        let output_packets = test_handleipv4_link(
+            vec![from_lan_to_me],
+            MY_WAN_IP,
+            MY_LAN_IP,
+            Cidr::new(MY_LAN_SUBNET_ADDR, MY_LAN_SUBNET_LEN).unwrap(),
+        );
+
+        assert_eq!(output_packets.len(), 1);
+    }
+
+    #[test]
+    fn from_lan_to_bcast_isnt_dropped() {
+        let mut from_lan_to_global_bcast_packet = Ipv4Packet::empty();
+        from_lan_to_global_bcast_packet.set_dest_addr(Ipv4Addr::new(255, 255, 255, 255));
+        let from_lan_to_global_bcast = InterfaceAnnotated {
+            packet: from_lan_to_global_bcast_packet,
+            inbound_interface: Interface::Lan,
+            outbound_interface: Interface::Unmarked,
+        };
+        let mut from_lan_to_local_bcast_packet = Ipv4Packet::empty();
+        let lan_cidr: Ipv4Cidr = Cidr::new(MY_LAN_SUBNET_ADDR, MY_LAN_SUBNET_LEN).unwrap();
+        from_lan_to_local_bcast_packet.set_dest_addr(lan_cidr.last_address());
+        let from_lan_to_local_bcast = InterfaceAnnotated {
+            packet: from_lan_to_local_bcast_packet,
+            inbound_interface: Interface::Lan,
+            outbound_interface: Interface::Unmarked,
+        };
+
+        let output_packets = test_handleipv4_link(
+            vec![from_lan_to_global_bcast, from_lan_to_local_bcast],
+            MY_WAN_IP,
+            MY_LAN_IP,
+            Cidr::new(MY_LAN_SUBNET_ADDR, MY_LAN_SUBNET_LEN).unwrap(),
+        );
+
+        assert_eq!(output_packets.len(), 2);
+    }
+
+    #[test]
+    fn from_lan_to_wan_isnt_dropped() {
+        let mut from_lan_to_wan_packet = Ipv4Packet::empty();
+        from_lan_to_wan_packet.set_dest_addr(Ipv4Addr::new(192, 0, 2, 7));
+        let from_lan_to_wan = InterfaceAnnotated {
+            packet: from_lan_to_wan_packet,
+            inbound_interface: Interface::Lan,
+            outbound_interface: Interface::Unmarked,
+        };
+
+        let output_packets = test_handleipv4_link(
+            vec![from_lan_to_wan],
+            MY_WAN_IP,
+            MY_LAN_IP,
+            Cidr::new(MY_LAN_SUBNET_ADDR, MY_LAN_SUBNET_LEN).unwrap(),
+        );
+        assert_eq!(output_packets.len(), 1);
     }
 }
